66cccdad6f48ee52feba39c315b09d5b0c6c25d2
Bellos0 <118614991+Bellos0@users.noreply.github.com>
2025-12-28 23:26:39 +0700

    Create Entity_AnimationEvent.cs

---
+++ Assets/Entity_AnimationEvent.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+/// <summary>
+/// script nay dung de marking cac diem enent trong animation de goi cac method tuong ung trong player.cs
+/// </summary>
+public class Entity_AnimationEvent : MonoBehaviour
+{
+    private Entity EntityService;
+
+    private void Awake()
+    {
+        EntityService = GetComponentInParent<Entity>(); // vi player nam trong parent ta dung getcomponentinparent de truy cap no
+    }
+
+   
+    /// <summary>
+    /// method nay duoc setup trong animation cua unity, khi o 1 frame nao do thi ta bat event nay len de goi method nay
+    /// </summary>
+    private void DisableMove_and_Jump()
+    {
+        EntityService.EndableMotion(false);
+    }
+    private void EndableMove_and_Jump()
+
+       => EntityService.EndableMotion(true); // viet kieu lamda expression, neu trong method chi co 1 dong code thi co the viet the nay cho nhanh
+    public void DamageTarget()
+    {
+    EntityService.DamageTarget();
+        //Debug.Log("player animation event damage enemy called");
+    }
+
+}
